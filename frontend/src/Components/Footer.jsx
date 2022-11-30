ecd7547155ca7248adc198c8f35579538cbbd13e
Thibault Adelain <thibault.adelain@gmail.com>
2022-11-30 20:30:29 +0100

    verification fields + UI update

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -5,20 +5,20 @@ function Footer() {
   return (
     <div className='containerFooter'>
         <footer className="p-4 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">Website made by Thibault Adelain
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 responsiveHide">Website made by Thibault Adelain
             </span>
             <ul className="flex flex-wrap items-center text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
-                    <Link to="/about" className="mr-4 hover:underline md:mr-6">About</Link>
+                    <Link to="/about" className="mr-4 hover:underline md:mr-6" id="about">About</Link>
                 </li>
                 <li>
-                    <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                    <a href="#" className="mr-4 hover:underline md:mr-6 responsiveHide">Privacy Policy</a>
                 </li>
                 <li>
-                    <a href="#" className="mr-4 hover:underline md:mr-6">Licensing</a>
+                    <a href="https://opensource.org/licenses/MIT" className="mr-4 hover:underline md:mr-6" id="licensing">Licensing</a>
                 </li>
                 <li>
-                    <a href="https://github.com/ThibaultAdelain" className="hover:underline">Contact</a>
+                    <a href="https://github.com/ThibaultAdelain" className="hover:underline" id="contact">Contact</a>
                 </li>
             </ul>
         </footer>
